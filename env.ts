2919c68d6f59e118b65afa590a021296dd0fb30b
stungph115 <82810693+stungph115@users.noreply.github.com>
2024-07-15 10:21:31 +0200

    Update env.ts

--- env.ts
+++ env.ts
@@ -5,13 +5,13 @@ export const env = {
     DATABASE_PASS: '',
     DATABASE_HOST: 'localhost',
     BASE_URL: 'http://localhost:3000/',
-    NETWORK_URL: 'http://192.168.1.154:3000',
+    NETWORK_URL: '',
     DATABASE_NAME: 'fundrisehub',
     USER_JWT_KEY: 'Xk1sOT8a94HDd55jV86mL',
     USER_PASSWORD_KEY: '89mq4ofYkVN1VbeO89Zt5',
     RESET_PASSWORD_KEY: 'JcKr9u4HS42sfGQebkhQR',
-    STRIPE_API_SECRET_KEY: 'sk_test_51OUmh2BBo1z4BCbnG4NlJycO5DQTjMOckZxGBDi9SQBaUgUaJ02mdeSVafVcN16TiVqJYuRmaD1ROnOo4Dilu0UO00Rh0rQA0b',
-    APP_PASS: 'okqrqjjjdlwubgea',
-    WEBHOOK_SECRET_STRIPE: 'whsec_8f5875a56bf8120cafe1df16de0d62bbfd5799d551d1c1b9aaba8abc54c0bb5a'
+    STRIPE_API_SECRET_KEY: '',
+    APP_PASS: '', //for gmail
+    WEBHOOK_SECRET_STRIPE: '' //stripe webhook
 
 }
